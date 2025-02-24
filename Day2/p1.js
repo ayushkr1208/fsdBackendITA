f838e10f02a8e5a298b7643196657847cb373db1
ayushkr1208 <ayushmaankr12@gmail.com>
2025-02-24 12:50:41 +0530

    Day2

---
+++ Day2/p1.js
@@ -0,0 +1,4 @@
+// const {sum,mul} = require('./myMath')/
+const obj=require('./myMath');
+console.log(obj.sum(2,3));
+console.log(obj.mul(2,3));
---
+++ Day2/p7.js
@@ -0,0 +1,26 @@
+const http = require('http');
+let users=[];
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    if(req.url==='/setdata' && req.method==='POST'){
+        let body='';
+        req.on('data', chunk => {
+            body += chunk;
+            });
+        req.on('end', () => {
+            const user = JSON.parse(body);
+            users.push(user);
+            res.end("Data Received");
+            });
+    }
+    else if(req.url==='/getdata' && req.method==='GET'){
+        res.end(JSON.stringify(users));
+    }
+    else{
+        res.end('Not found');
+    }
+});
+
+server.listen(9000,()=>{
+    console.log('Server is running on port 9000');
+});
